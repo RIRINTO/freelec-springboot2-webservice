e8f7f110206dae90db1f73bb278a7119a122515a
이학재 <ririnto@kakao.com>
2021-12-19 23:47:14 +0900

    Initial commit

---
+++ src/main/java/com/ririnto/book/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.ririnto.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @EnableJpaAuditing가 삭제됨
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
